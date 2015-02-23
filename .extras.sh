34ff3987a60f74b022de8ad43bce2ebdcc4523a6
Joe Rice <ricje7@gmail.com>
2015-02-22 19:08:16 -0500

    commit extras and custom

---
+++ .extras.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+brew install caskroom/cask/brew-cask
+
+sudo brew cask install --appdir="/Applications" alfred
+sudo brew cask install --appdir="/Applications" dropbox
+sudo brew cask install --appdir="/Applications" dropbox
+sudo brew cask install --appdir="/Applications" phpstorm
+sudo brew cask install --appdir="/Applications" utorrent
+sudo brew cask install --appdir="/Applications" vlc
+sudo brew cask install --appdir="/Applications" google-chrome
+sudo brew cask install --appdir="/Applications" google-drive
+sudo brew cask install --appdir="/Applications" textwrangler
+sudo brew cask install --appdir="/Applications" dash
+sudo brew cask install --appdir="/Applications" caffeine
+sudo brew cask install --appdir="/Applications" totalterminal
+sudo brew cask install --appdir="/Applications" virtualbox
+sudo brew cask install --appdir="/Applications" vagrant
+
+
+npm install -g bower
+npm install -g grunt-cli
+npm install -g yo
+npm install -g nw
+npm install -g node-webkit-builder
+npm install -g generator-node-webkit
+npm install -g generator-angular
